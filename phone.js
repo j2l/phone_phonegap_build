c5ccf5b888fb4f9b3066a49fb481dc13703d9380
j2l <nothanks@nowhere.no>
2012-11-05 21:54:16 +0100

    first commit

---
+++ phone.js
@@ -0,0 +1,29 @@
+dojo.declare("phone", wm.Application, {
+	"disableDirtyEditorTracking": false, 
+	"eventDelay": 0, 
+	"i18n": false, 
+	"isSecurityEnabled": false, 
+	"main": "Main", 
+	"manageHistory": true, 
+	"manageURL": false, 
+	"name": "", 
+	"phoneGapLoginPage": "Login", 
+	"phoneMain": "", 
+	"projectSubVersion": "Alpha", 
+	"projectVersion": 1, 
+	"studioVersion": "6.5.0.RELEASE", 
+	"tabletMain": "", 
+	"theme": "wm_default", 
+	"toastPosition": "br", 
+	"touchToClickDelay": 500, 
+	"touchToRightClickDelay": 1500,
+	"widgets": {
+		silkIconList: ["wm.ImageList", {"colCount":39,"height":16,"iconCount":90,"url":"lib/images/silkIcons/silk.png","width":16}, {}]
+	},
+	_end: 0
+});
+
+phone.extend({
+
+	_end: 0
+});
